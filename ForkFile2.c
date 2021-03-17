0d6cd4b6a5a96ac759b05bc812de6e8a082bcb2b
Bjørnar Sømme <bjorsom@icloud.com>
2021-03-17 17:17:44 +0100

    YESSIR

---
+++ ForkFile2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main(){
+    fork();
+fork();
+fork();
+fork();
+    printf("hello, my name is Bjornur Schwomme");
+    return 0;
+}
